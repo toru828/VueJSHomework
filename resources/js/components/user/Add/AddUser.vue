877677c025a51db35070d9af7fc5a7e9ce0f5337
長谷川 徹 <tooru.hasegawa@pasnet.local>
2021-08-25 19:08:30 +0900

    for mastser

---
+++ resources/js/components/user/Add/AddUser.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-btn color="primary" @click="onClickAddButton()">
+        Add User
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "AddUser",
+    data() {
+        return {};
+    },
+    methods: {
+        onClickAddButton() {
+            this.$router.push("/add");
+        }
+    }
+};
+</script>
---
+++ resources/js/components/user/Edit/EditUser.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-btn color="primary" @click="onClickEditButton(userID)" small>
+        Edit
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "EditUser",
+    data() {
+        return {};
+    },
+    props: ["userID"],
+    methods: {
+        onClickEditButton(id) {
+            this.$router.push("/edit/" + id);
+        }
+    }
+};
+</script>
